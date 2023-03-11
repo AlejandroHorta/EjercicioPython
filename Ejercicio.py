900d670ebebbc2826a7fd2cb818abdf34cf8b1d9
Alejandro Restrepo Horta <alejandrohort@hotmail.com>
2023-03-10 20:42:45 -0500

    Se agrega el proyecto

---
+++ Ejercicio.py
@@ -0,0 +1,4 @@
+ciudades = ["Medellín", "Bogota", "Cali", "Pereira", "Cartagena", "Barranquilla", "Armenia", "Leticia"]  #Arreglo de objetos con 8 ciudades
+longitud = len(ciudades) #Iterador longitud que recorre el arreglo de objetos
+for item in reversed(ciudades): #Función reversed
+    print(item)
